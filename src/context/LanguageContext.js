fc8e6f1ab5b29d9fee9e69685bae9e5bd74bfc41
sampurnv <sampurnv@yahoo.com>
2025-12-01 14:59:38 +0530

    Add multi-language context

---
+++ src/context/LanguageContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useState, useContext } from 'react';
+import translations from '../translations';
+
+const LanguageContext = createContext();
+
+export const useLanguage = () => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error('useLanguage must be used within LanguageProvider');
+  }
+  return context;
+};
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState(() => {
+    return localStorage.getItem('language') || 'en';
+  });
+
+  const changeLanguage = (lang) => {
+    setLanguage(lang);
+    localStorage.setItem('language', lang);
+  };
+
+  const t = (key) => {
+    return translations[language]?.[key] || key;
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage, t }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export default LanguageContext;
